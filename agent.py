03f72c55055cd33fa59b691f7268c476b5666ac4
Evan Lamb <evanlamb848@gmail.com>
2025-08-22 10:56:54 -0400

    updating to enable tool calling

--- agent.py
+++ agent.py
@@ -4,29 +4,43 @@ from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import BaseMessage
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.chat_models import init_chat_model
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
+
+tools = []
 
 graph_builder = StateGraph(State)
 
 
 llm = init_chat_model("openai:gpt-4.1")
 
+llm_with_tools = llm.bind_tools(tools)
+
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+tool_node = ToolNode(tools=[tools])
 
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
+
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
+
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+
 
 graph = graph_builder.compile()
 
